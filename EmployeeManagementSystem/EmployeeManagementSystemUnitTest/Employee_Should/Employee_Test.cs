aaa26a9c550ac156f692dd5625c1004bff92d24e
Diyan Lyubchev <diyan.lyubchev@gmail.com>
2020-02-10 12:58:10 +0200

    Added tests

--- EmployeeManagementSystem/EmployeeManagementSystemUnitTest/Employee_Should/Employee_Test.cs
+++ EmployeeManagementSystem/EmployeeManagementSystemUnitTest/Employee_Should/Employee_Test.cs
@@ -115,5 +115,80 @@ namespace EmployeeManagementSystemUnitTest.Employee_Should
             }
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(EmployeeException))]
+        public async Task ThrowException_IfExperienceEmployeeIdIsNotCorrect()
+        {
+            var employee = EmployeeGeneratorUtil.GenerateEmployee();
+
+
+            var dto = new EmployeeDto
+            {
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                CompanyId = employee.CompanyId,
+                OfficeId = employee.OfficeId,
+                ExperienceEmployeeId = 0
+            };
+
+            var options = TestUtilities.GetOptions(nameof(ThrowException_IfExperienceEmployeeIdIsNotCorrect));
+
+            using (var actContext = new EmployeeManagementSystemContext(options))
+            {
+                var sut = new EmployeeService(actContext);
+                var resilt = await sut.AddAsync(dto);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmployeeException))]
+        public async Task ThrowException_IfCompanyIdIsNotCorrect()
+        {
+            var employee = EmployeeGeneratorUtil.GenerateEmployee();
+
+            var dto = new EmployeeDto
+            {
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                CompanyId = 0,
+                OfficeId = employee.OfficeId,
+                ExperienceEmployeeId = employee.ExperienceEmployeeId,
+                 
+            };
+
+            var options = TestUtilities.GetOptions(nameof(ThrowException_IfCompanyIdIsNotCorrect));
+
+            using (var actContext = new EmployeeManagementSystemContext(options))
+            {
+                var sut = new EmployeeService(actContext);
+                var resilt = await sut.AddAsync(dto);
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmployeeException))]
+        public async Task ThrowException_IfOfficeIsDeleted()
+        {
+            var employee = EmployeeGeneratorUtil.GenerateEmployee();
+            var office = OfficeGeneratorUtil.GenerateOffice();
+            office.IsDeleted = true;
+            var dto = new EmployeeDto
+            {
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                CompanyId = 0,
+                OfficeId = office.Id,
+                ExperienceEmployeeId = employee.ExperienceEmployeeId,
+            };
+
+            var options = TestUtilities.GetOptions(nameof(ThrowException_IfOfficeIsDeleted));
+
+            using (var actContext = new EmployeeManagementSystemContext(options))
+            {
+                var sut = new EmployeeService(actContext);
+                var resilt = await sut.AddAsync(dto);
+            }
+        }
+
     }
 }
